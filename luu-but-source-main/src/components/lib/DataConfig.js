63dfe4670bb1f8f400bf715e95194afc8b77d323
longkinhcan <138272578+longkinhcan@users.noreply.github.com>
2023-12-02 21:46:01 +0700

    Add files via upload

---
+++ luu-but-source-main/src/components/lib/DataConfig.js
@@ -0,0 +1,62 @@
+const dc = {
+    myself: "Tớ",
+    //đièn tên và gmail của cậu vào đây nha
+    email: "nongvanson45@gmail.com",
+    meta: {
+        //hiển thị lúc share bài lên fb
+        img: "https://www.macmillandictionary.com/external/slideshow/thumb/Grey_thumb.png",
+    },
+    headCard: {
+        //title có tên rồi nha
+        title: "Lưu bút của Lê Long",
+        image: "https://www.macmillandictionary.com/external/slideshow/thumb/Grey_thumb.png",
+        content: "Mọi người viết nội dung vào đây nhé",
+        content2: "Có gì không hiểu có thể hỏi tớ nè!",
+        button1: "Bắt đầu thui",
+        button2: "Đọc lại thư của cậu",
+    },
+    nameInput: {
+        title: "Tên của cậu là gì nè?",
+        subtitle: "Sửa dòng này nữa nha",
+        nameError: "*Điền tên cậu vào iii",
+        placeholder: "Tên cậu nè...",
+    },
+    aboutMe: {
+        title: "Cậu có cảm nhân gì về tớ khong?",
+        subtitle: "Sửa dòng này nữa nha",
+        aboutError: "*Nói gì iii",
+        placeholder: "Tớ nghĩ cậu...",
+    },
+    handsome: {
+        title: "Cậu đánh giá tớ thế nào?",
+        subtitle: "Sửa dòng này nữa nha",
+        label: {
+            one: "Tồi",
+            fouth: "Hong bit",
+            sixth: "Ok",
+            full: "Đỉnh",
+        }
+    },
+    memories: {
+        title: "Chúng mình có kỉ niệm gì với nhau khong?",
+        subtitle: "Sửa dòng này nữa nha",
+        memoriesError: "*Hong có gì àaa",
+        placeholder: "Kỉ niệm nè...",
+    },
+    message: {
+        title: "Cậu có muốn nói gì với tớ khong?",
+        subtitle: "Sửa dòng này nữa nha",
+        messageError: "Hong gửi gì àaa",
+        placeholder: "Gửi di nèee...",
+    },
+    submit: {
+        title: "Vậy là xong ròi nèee!!!",
+        subheader: "Nhớ thay thumbnail nha",
+        image: "https://www.macmillandictionary.com/external/slideshow/thumb/Grey_thumb.png",
+        content: "Cái này sau khi gửi nè",
+        content2:"Nhớ điền captcha nha 🤖.",
+        button: "Gửi cho tớ lunnn",
+    },
+}
+
+export default dc;
